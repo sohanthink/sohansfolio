94fcb133c06220c82857eaf7ed9234b83d0a2834
Muhammad Sohan Mollah <sohansourab@gmail.com>
2024-09-30 20:32:20 +0600

    using bento grid for about us section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 
 const Home = () => {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className=" bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        {/* grid background */}
         <Hero/>
+        <Grid/>
       </div>
     </main>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -20,7 +20,6 @@ const Hero = () => {
         <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
       </div>
 
-      {/* grid background */}
       <div className="h-screen w-full dark:bg-black bg-white dark:bg-grid-white/[0.06] bg-grid-black/[0.2] absolute top-0 left-0 flex items-center justify-center z-0">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
