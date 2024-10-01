5f1164a53ad5b3a229fdc75bc1a5c15d962d6151
Muhammad Sohan Mollah <sohansourab@gmail.com>
2024-10-01 14:41:35 +0600

    bento grid updated with glove/image/spare /gradient background and etc modifications

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,4 +1,8 @@
 import { cn } from "@/lib/utils";
+import { BackgroundGradientAnimation } from "./GradientBg";
+import { div } from "framer-motion/client";
+import {GridGlobe} from "./GridGlobe";
+import { MotionGlobalConfig } from "framer-motion";
 
 export const BentoGrid = ({
   className,
@@ -54,14 +58,45 @@ export const BentoGridItem = ({
       }}
     >
 
+        <div className={`${id === 6 && 'flex justify-center'}`}>
+            <div className="w-full h-full relative">
+                {img &&(
+                    <img src={img} alt={img} className={cn(imgClassName, 'object-cover object-center')} />
+                )}
+            </div>
+            {/* <div className={`absolute right-0 -bottom-5 ${id === 5 && "w-full opacity-80"} `}/> */}
+            <div>
+                {
+                    spareImg && (
+                        <img src={spareImg} alt={spareImg} className='object-cover object-center h-full w-full' />
+                    )
+                }
+            </div>
+        </div>
+
+        {
+            id === 6 && (
+                <BackgroundGradientAnimation className="">
+                    <div className="h-32 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
+                </BackgroundGradientAnimation>
+            )
+        }
+
       <div className="group-hover/bento:translate-x-2 transition duration-200">
         <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
           {title}
         </div>
-        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
+        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300 ">
           {description}
         </div>
       </div>
+
+      {
+            id === 2 && (
+                <GridGlobe />
+            )
+        }
+
     </div>
   );
 };
